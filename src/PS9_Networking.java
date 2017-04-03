d41ce66b9aa80ae1289f2fdc86d9ea1843ff213e
jessehartloff <hartloff@buffalo.edu>
2017-04-03 10:36:10 -0400

    initial commit

---
+++ src/PS9_Networking.java
@@ -0,0 +1,123 @@
+
+public class PS9_Networking {
+
+    static String Q1(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api and return the server's response
+        // as a String
+        //
+        // Note: There will be very little feedback from Autolab for this assignment. You should run your code
+        //       to verify that you are recieving a response from the server before sumbitting.
+
+        return "";
+    }
+
+    static String Q2(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_string and return the server's response
+        // as a String
+
+        return "";
+    }
+
+    static int Q3(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_int and return the server's response
+        // as an int. You may assume that the response is a well-formed integer
+
+        return 0;
+    }
+
+    static String Q4(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/post and return the
+        // server's response as a String
+
+        return "";
+    }
+
+    static String Q5(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params and return the
+        // server's response as a String
+
+        return "";
+    }
+
+    static String Q6(){
+        // Send the empty String (eg. "") in the body of an HTTP POST request to the url
+        // http://fury.cse.buffalo.edu/api/post and return the server's response as a String. Use
+        // DEFAULT_TEXT as the content type.
+
+        return "";
+    }
+
+    static String Q7(){
+        // Send "hello server" in the body of an HTTP POST request to the url
+        // http://fury.cse.buffalo.edu/api/post and return the server's response as a String. Use
+        // DEFAULT_TEXT as the content type.
+
+        return "";
+    }
+
+    static String Q8(){
+        // Send "how are you?" in the body of an HTTP POST request to the url
+        // http://fury.cse.buffalo.edu/api/post and return the server's response as a String. Use
+        // DEFAULT_TEXT as the content type.
+
+        return "";
+    }
+
+    static String Q9(String content){
+        // Send the provided content in the body of an HTTP POST request to the url
+        // http://fury.cse.buffalo.edu/api/post and return the server's response as a String. Use
+        // DEFAULT_TEXT as the content type.
+
+        return "";
+    }
+
+    static String Q10(Integer value){
+        // Send the provided int value in the body of an HTTP POST request to the url
+        // http://fury.cse.buffalo.edu/api/post and return the server's response as a String. Use
+        // DEFAULT_TEXT as the content type.
+
+        return "";
+    }
+
+    static String Q11(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params with a
+        // parameter "question" set to the value "11" and return the server's response as a String
+
+        return "";
+    }
+
+    static String Q12(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params with a
+        // parameter "key" set to the value "value" and return the server's response as a String
+
+        return "";
+    }
+
+    static String Q13(String param){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params with a
+        // parameter "key" set to the value of the provided param and return the server's response as a String
+
+        return "";
+    }
+
+    static String Q14(){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params with a
+        // parameter "question" set to the value "14" and a parameter "key" set to the value "turtle" and return
+        // the server's response as a String
+
+        return "";
+    }
+
+    static String Q15(String param1, String param2){
+        // Send an HTTP GET request to the url http://fury.cse.buffalo.edu/api/get_with_params with a
+        // parameter "key" set to the value of param1 and a parameter "query" set to the value of param2
+        // and return the server's response as a String
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
